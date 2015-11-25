05dcc522b23be83e33eec362d3a788dd8e4b098c
andresombra <aadsce@gmal.com>
2015-11-25 15:53:10 -0300

    Atualizacao 2 25112015

--- Easydoc/WebApp/Assets/js/app-scripts/funcoes.js
+++ Easydoc/WebApp/Assets/js/app-scripts/funcoes.js
@@ -17,3 +17,13 @@ function exibirmsgatencao(txtmsg) {
     });
 }
 
+function DataAtual() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+    var today = year + "-" + month + "-" + day; //Retorna nesse formato: yyyy-MM-dd 
+    return today;
+}
